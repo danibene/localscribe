b8cbd092b5baef24ff0d803286641d9459e36cf6
D. Benesch <34680344+danibene@users.noreply.github.com>
2026-03-05 13:22:56 -0500

    Update test_skeleton.py to mock transcribe_audio function

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,14 +1,16 @@
+from unittest.mock import patch
 from localscribe.skeleton import main
 
 __author__ = "danibene"
 __copyright__ = "danibene"
 __license__ = "MIT"
 
-
 def test_main(capsys):
     """CLI Tests"""
     # capsys is a pytest fixture that allows asserts against stdout/stderr
     # https://docs.pytest.org/en/stable/capture.html
-    main(["7"])
-    captured = capsys.readouterr()
-    assert "crazy" in captured.out
+    with patch('localscribe.skeleton.transcribe_audio') as mock_transcribe:
+        main(["7"])
+        captured = capsys.readouterr()
+        assert "crazy" in captured.out
+        mock_transcribe.assert_called_once_with("7")
